81daab0f9a746c40461c7ff79742256af313ed08
Infendro <fabianschoenberger2301@gmail.com>
2020-10-06 14:50:07 +0200

    implemented two methods in class "Streams"

---
+++ Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/RandomStringArray.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgrieskirchen.pos3.streams;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Fabian
+ */
+public class RandomStringArray {
+    public String[] getRandomStringArray(int amount,int length){
+        return (String[]) Arrays.stream(new String[amount])
+                .map(string -> getRandomString(length))
+                .toArray();
+    }
+    
+    private String getRandomString(int length){
+        String string = "";
+        for (int i = 0; i < length; i++) {
+            string += getRandomASCIICharacter();
+        }
+        return string;
+    }
+    
+    private char getRandomASCIICharacter(){
+        return (char) ((int)(Math.random()*26)+26);
+    }
+}
--- Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Streams.java
+++ Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Streams.java
@@ -6,12 +6,24 @@ import java.util.stream.Collectors;
 
 public class Streams {
 
+    private int[] numbers;
+    private String[] strings;
+    
+    public Streams(){
+        numbers = new RandomNumberArray().getRandomNumberArray(1000, 0, 100);
+        strings = new RandomStringArray().getRandomStringArray(10,10);
+    }
+    
     public double average(int[] numbers) {
-        //implement this
+        return Arrays.stream(numbers)
+                .average()
+                .getAsDouble();
     }
     
     public List<String> upperCase(String[] strings) {
-        //implement this
+        return Arrays.stream(strings)
+                .map(string -> string.toUpperCase())
+                .collect(Collectors.toList());
     }
     
     public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
